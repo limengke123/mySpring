99aee570ad5a85b13df4f6a8da3b07b0519441dc
李梦克 <limengke@souche.com>
2018-12-10 15:54:23 +0800

    :tada: 能够返回json数据

--- src/main/java/com/limengke/mySpring/controller/UserController.java
+++ src/main/java/com/limengke/mySpring/controller/UserController.java
@@ -5,11 +5,13 @@ import com.limengke.mySpring.model.User;
 import com.limengke.mySpring.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/user")
@@ -31,5 +33,16 @@ public class UserController {
         ObjectMapper mapper = new ObjectMapper();
         response.getWriter().write(mapper.writeValueAsString(user));
         response.getWriter().close();
+    }
+
+    @RequestMapping("showMyUser.do")
+    @ResponseBody
+    public User selectUser1 (
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) throws IOException {
+        long userId = Long.parseLong(request.getParameter("id"));
+        User user = userService.selectUser(userId);
+        return user;
     }
 }
